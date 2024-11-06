ae7b97cc7570451193d9937aba172bb5347352e6
JaStaGa <jaylinstarlinganaway@gmail.com>
2024-11-05 20:23:37 -0500

    skeleton

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript is connected and ready!");
+    // Here’s where we’ll add code to fetch player data
+});
