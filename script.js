c514288e6acf116d03a4a2f6e6c1134c80be11ef
JaStaGa <jaylinstarlinganaway@gmail.com>
2024-11-07 15:01:49 -0500

    handling form submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("JavaScript is connected and ready!");
-    // Here’s where we’ll add code to fetch player data
-});
+
+    const searchForm = document.getElementById("search-form");
